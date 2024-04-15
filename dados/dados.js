64ad0b27b08065153546fe7c4885a8c165287d97
ICI-INC <166365607+ICI-INC@users.noreply.github.com>
2024-04-15 00:20:52 -0300

    icitv

--- dados/dados.js
+++ dados/dados.js
@@ -3,7 +3,7 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if(login = "1" && senha == "14298156"){ //yasmin
+    if(login = "1" && senha == "1"){ //yasmin
         alert('Sucesso');
         location.href = "https://ici-inc.github.io/icitv/home.html"; 
     }else{
