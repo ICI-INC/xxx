5c841482f1a86c451f6870ae3664f1149b6eb0d7
ICI-INC <166365607+ICI-INC@users.noreply.github.com>
2025-01-28 20:13:00 -0300

    Update dados.js

--- dados/dados.js
+++ dados/dados.js
@@ -12,7 +12,7 @@ function logar(){
 
     if(login = "1" && senha == "14298156"){ //marcelo
         alert('Sucesso');
-        location.href = "https://ici-inc.github.io/icitv/assets/icitv.htmll";
+        location.href = "https://ici-inc.github.io/icitv/assets/icitv.html";
     }else{
         
     }
